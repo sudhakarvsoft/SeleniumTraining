e48bd18d6077c58aeeadbb9b6d5e4fb63d51c248
skakunuri <skakunuri@VSCLPTEST10.corp.vsoftconsulting.com>
2018-10-30 16:25:32 +0530

    Frames

--- TrainingProject1/src/com/wd/basics/Wd16WorkingWithDuplicates.java
+++ TrainingProject1/src/com/wd/basics/Wd16WorkingWithDuplicates.java
@@ -12,27 +12,25 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import com.wd.config.BrowserConfig;
 import com.wd.util.DriverFactory;
 
-public class Wd15WorkingWithFrames {
+public class Wd16WorkingWithDuplicates {
 
 	public static void main(String[] args) throws InterruptedException {
 
 		WebDriver driver = DriverFactory.getDriverFor(BrowserConfig.testBrowser);
 		driver.manage().window().maximize();
-		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		driver.get("https://www.redbus.in/");
 
-		driver.findElement(By.id("signin-block")).click();
-		driver.findElement(By.id("signInLink")).click();
-		
-		WebElement frm = driver.findElement(By.className("modalIframe"));
-		driver.switchTo().frame(frm); //frame scope
-		
-		driver.findElement(By.id("mobileNoInp")).sendKeys("9876543210");
-		
-		driver.switchTo().defaultContent(); //main page scope
-		
+		for (int i = 1; i <= 5; i++) {
+			driver.findElement(By.id("signin-block")).click();
+			driver.findElement(By.id("signInLink")).click();
+
+			Thread.sleep(2000);
+
+			driver.findElement(By.xpath("descendant::div[@class='modalCloseSmall'][last()]")).click();
+		}
 	}
 
 }
